2f513742760fcdac6f1dd32feaaa1c02efd9754b
projesh basnet <projeshbasnet@gmail.com>
2023-06-07 08:26:04 +0545

    first day

---
+++ index.js
@@ -0,0 +1,68 @@
+// concept of let, const and var
+// These are used in scoping variables
+
+// const x = 5
+// x = 7
+
+// implementation of let
+
+// let letVariable = 5
+// {
+//     let letVariable = 10
+//     console.log("inside scope letVariable", letVariable)
+// }
+
+// console.log("outside scope letVariable", letVariable)
+
+
+// implementation of var
+
+// var varVariable = 18
+// {
+//     var varVariable = 81
+//     console.log("inside scope varVariable",varVariable)
+// }
+
+// console.log("outside scope varVariable",varVariable)
+
+// concept of Callback function
+// function greet(name, callback) {
+//     console.log("Hello, " + name + "!");
+
+//     // Call the callback function
+//     callback(name);
+//   }
+
+//   function sayGoodbye(name) {
+//     console.log("Goodbye! ",name);
+//   }
+
+// question: create a callback function
+
+
+//   // Pass the sayGoodbye function as a callback to the greet function
+//   greet("John", sayGoodbye);
+
+
+// forloop, while loop
+
+// for (let i = 1; i <= 5; i++) {
+//     console.log(i);
+// }
+
+// let arr = [10,12,14,45,30]
+// arr.forEach((num)=>{
+//     console.log(num)
+// })
+
+
+
+
+
+
+
+
+
+
+
+
